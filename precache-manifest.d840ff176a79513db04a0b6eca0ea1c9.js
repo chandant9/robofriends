a194b0288565f0261b274e5766087658d3d2b6bd
chandant9 <discoverchan9@gmail.com>
2020-05-27 14:59:12 +0400

    Updates

---
+++ precache-manifest.d840ff176a79513db04a0b6eca0ea1c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d23ccb52c3357eeac9bca5991d4257bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f08c14dbaf972f2dc072",
+    "url": "/static/css/main.2cf75415.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f08c14dbaf972f2dc072",
+    "url": "/static/js/main.412a802d.chunk.js"
+  },
+  {
+    "revision": "fdb66bf0534857f19626",
+    "url": "/static/js/runtime-main.8f3ae637.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
